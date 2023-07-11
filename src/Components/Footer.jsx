93b1153427640ebcd33d600adb80787b3ba9b57c
Ammar <bhatmmr@gmail.com>
2023-07-11 17:53:31 +0530

    added useEffect

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className='w-full pt-36 mt-10 h-48 bg-gray-700 text-white text-center font-semibold mb-0 pt-6'>Developed and designed by Ammar Qayoom</div>
+    <div className='w-full pt-36 mt-10 h-48 bg-gray-700 text-white text-center font-semibold mb-0 '>Developed and designed by Ammar Qayoom</div>
   )
 }
 
