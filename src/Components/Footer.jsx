104bc4f494b3fc246f67239f7b96abbc3dc117ec
Ammar <bhatmmr@gmail.com>
2023-07-16 13:06:06 +0530

    fixing selecth

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className=' pt-20 mt-10 h-48 bg-gray-700 text-white text-center font-semibold mb-0 '>Developed and designed by Ammar Qayoom and 
+    <div className=' py-16 mt-10 h-36 bg-gray-700 text-white text-center font-semibold mb-0 '>Developed and designed by Ammar Qayoom and 
      <a href="https://murtazajoo.me/" className='text-blue-300 hover:underline pl-1'>Murtaza Joo</a></div>
   )
 }
--- src/Components/Search/SearchR.jsx
+++ src/Components/Search/SearchR.jsx
@@ -6,8 +6,8 @@ function SearchR(props) {
 
     return (
        <>
-       <div className='text-center text-4xl p-10'>{props.name}</div>
-       <div className='p-6 bg-yellow-200 rounded-xl text-gray-800 mx-10 sm:mx-20'>
+       <div className='text-center text-4xl '>{props.name}</div>
+       <div className='p-6 bg-yellow-200 rounded-xl text-gray-800 m-10 sm:mx-20'>
            <div className='text-xl '>Meanings</div>
            <div className='p-2'>{props.def}</div>
        </div>
