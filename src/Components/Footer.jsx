1a5dec6ac073d025d293840d80f561b14b72571a
ammarbhat <bhatmmr@gmail.com>
2024-08-20 11:58:41 +0530

    design chnages

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className=' py-16 mt-10 h-36 bg-gray-700 text-white text-center font-semibold mb-0 '>Developed and designed by Ammar Qayoom and 
+    <div className=' py-16 mt-10 h-20 w-full absolute bottom-0 bg-gray-700 text-white text-center font-semibold mb-0 '>Developed and designed by Ammar Qayoom and 
      <a href="https://murtazajoo.me/" className='text-blue-300 hover:underline pl-1'>Murtaza Joo</a></div>
   )
 }
